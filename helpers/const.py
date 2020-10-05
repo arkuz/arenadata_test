391d365cea33446b1f79b860451ed96142a0e61b
Artem Kuzmenko <arkuzka@mail.ru>
2020-10-05 15:14:21 +0300

    Почистил хелперы

--- helpers/const.py
+++ helpers/const.py
@@ -4,5 +4,5 @@ from helpers.tools import get_project_path
 
 
 # Пути к директориям
-PROJECT = get_project_path(os.getcwd()) # корневая папка проекта
-
+PROJECT = get_project_path(os.getcwd())  # корневая папка проекта
+TEST_DATA = os.path.join(PROJECT, 'test_data')  # папка с тестовыми данными
