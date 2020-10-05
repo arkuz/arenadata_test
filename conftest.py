b5551452ef5306ddd79fa2503355bf47ea1b4020
Artem Kuzmenko <arkuzka@mail.ru>
2020-10-05 22:44:23 +0300

    Изменил conftest

--- conftest.py
+++ conftest.py
@@ -4,6 +4,8 @@ import pytest
 import helpers.const as const
 from helpers.api import API
 from helpers.readers import read_yaml
+from selene.support.shared import browser
+
 
 config = read_yaml(os.path.join(const.PROJECT, 'config.yaml'))
 api_url = config['api_url']
@@ -27,3 +29,26 @@ def upload_test_templates():
         name = f'{num}.yaml'
         filename = os.path.join(const.TEST_DATA, name)
         api.upload_template(filename=filename)
+
+
+@pytest.fixture()
+def install_template_4():
+    templ_id = '4'
+    file = '4.yaml'
+    api.insall_template(templ_id)
+    return read_yaml(os.path.join(const.TEST_DATA, file))
+
+
+@pytest.fixture()
+def upload_empty_template():
+    name = 'empty.yaml'
+    filename = os.path.join(const.TEST_DATA, name)
+    api.upload_template(filename=filename)
+
+
+@pytest.fixture()
+def start_chrome():
+    config = read_yaml(os.path.join(const.PROJECT, 'config.yaml'))
+    site_url = config['site_url']
+    browser.config.browser_name = 'chrome'
+    browser.open(site_url)
