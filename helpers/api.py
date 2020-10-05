0afefe3836f05828b165719bedb5d0af632aa23b
Artem Kuzmenko <arkuzka@mail.ru>
2020-10-05 15:13:53 +0300

    Добавил класс для работы с API

--- helpers/api.py
+++ helpers/api.py
@@ -1,24 +1,33 @@
 import requests as req
 
 
-class API(object):
+class API():
     """ Класс описывает endpoints API """
 
     def __init__(self, url):
         self.url = url
 
 
-    def call_post_api(self, url, data={}, params={}):
-        """ Обертка над requests.post """
-        resp = req.post(
+    def _call_put_api(self, url, files, data={}):
+        """ Обертка над requests.put """
+        resp = req.put(
             url,
+            files=files,
             data=data,
-            params=params,
         )
         return resp
 
 
-    def call_get_api(self, url, params={}):
+    def _call_post_api(self, url, data={}):
+        """ Обертка над requests.post """
+        resp = req.post(
+            url,
+            data=data,
+        )
+        return resp
+
+
+    def _call_get_api(self, url, params={}):
         """ Обертка над requests.get """
         resp = req.get(
             url,
@@ -27,7 +36,7 @@ class API(object):
         return resp
 
 
-    def call_delete_api(self, url, params={}, data={}):
+    def _call_delete_api(self, url, params={}, data={}):
         """ Обертка над requests.delete """
         resp = req.delete(
             url,
@@ -37,20 +46,26 @@ class API(object):
         return resp
 
 
-    def add_subscriber(self, data={}):
-        endpoint = '/subscriptions'
+    def upload_template(self, filename, data={}):
+        files = {'file': open(filename, 'rb')}
+        endpoint = '/templates'
         url = self.url + endpoint
-        return self.call_post_api(url, data=data)
+        return self._call_put_api(url, files=files, data=data)
 
 
-    def get_subscribers(self):
-        endpoint = '/subscriptions'
+    def get_templates(self):
+        endpoint = '/templates'
         url = self.url + endpoint
-        return self.call_get_api(url)
+        return self._call_get_api(url)
 
 
-    def delete_subscribers(self):
-        endpoint = '/subscriptions'
+    def delete_template(self, tmpl_id):
+        endpoint = f'/templates/{tmpl_id}'
         url = self.url + endpoint
-        return self.call_delete_api(url)
+        return self._call_delete_api(url)
 
+
+    def insall_template(self, tmpl_id):
+        endpoint = f'/templates/{tmpl_id}/install'
+        url = self.url + endpoint
+        return self._call_post_api(url)
