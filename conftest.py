6f6bbc3ebd8a94831addb89ed4f9560459ac58c4
Artem Kuzmenko <arkuzka@mail.ru>
2020-10-05 15:19:12 +0300

    Изменил conftest

--- conftest.py
+++ conftest.py
@@ -1,30 +1,29 @@
-import json
 import os
 import pytest
 
 import helpers.const as const
 from helpers.api import API
 from helpers.readers import read_yaml
-from helpers.tools import generate_random_string, generate_email
-
 
 config = read_yaml(os.path.join(const.PROJECT, 'config.yaml'))
 api_url = config['api_url']
 api = API(api_url)
 
 
-@pytest.fixture
-def add_subscriber_fixture():
-    def _method(count=1):
-        result_list = []
-        for i in range(0, count):
-            data = {
-                'email': generate_email(),
-                'name': generate_random_string(),
-                'time': '7d',
-            }
-            api.add_subscriber(json.dumps(data))
-            result_list.append(data)
-        return result_list
-    return _method
+def pytest_make_parametrize_id(val):
+    return repr(val)
 
+
+@pytest.fixture()
+def delete_all_templates():
+    templates = api.get_templates().json()["templates"]
+    for name in templates:
+        api.delete_template(name)
+
+
+@pytest.fixture()
+def upload_test_templates():
+    for num in range(1, 5):
+        name = f'{num}.yaml'
+        filename = os.path.join(const.TEST_DATA, name)
+        api.upload_template(filename=filename)
